ae518cf9a49ba4a79a1e2f01b199b2cba73a1a35
c-c-k <c-c-k@nym.hush.com>
2023-02-10 15:24:10 +0200

    Fixed admin site registration.

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from .models import Author, Book, BookCopy, Genre, Language
 
-admin.register(Author)
-admin.register(Book)
-admin.register(BookCopy)
-admin.register(Genre)
-admin.register(Language)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookCopy)
+admin.site.register(Genre)
+admin.site.register(Language)
