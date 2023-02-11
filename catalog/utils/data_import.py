77e61a8ad203a170353d6ea2f14c7e08e392c479
c-c-k <c-c-k@nym.hush.com>
2023-02-11 14:51:42 +0200

    data_import.py wip.

--- catalog/utils/data_import.py
+++ catalog/utils/data_import.py
@@ -1,23 +1,93 @@
 import csv
 import datetime
+from decimal import Decimal
 from pathlib import Path
 from pprint import pprint
 from random import randint
 
-BOOK_SOURCE = Path('./classics.csv')
-BOOK_FILTERED = Path('./books.csv')
-CLIENT_SOURCE = Path('./customers-100.csv')
-CLIENT_FILTERED = Path('./clients.csv')
-LOAN_DATA = Path('./loans.csv')
+from ..models import Author, Categories, Book, BookCopy
+
+DATASET_ROOT = Path(__file__).parent / 'datasets/'
+DATASET_7K_BOOKS = DATASET_ROOT / 'books_7k.csv'
 
 
-def print_csv_headers(csv_file: Path):
+def get_sample_entry_from_csv(csv_file: Path, print_=False, temp_file=True):
+    """Get a sample entry to use as reference while writing importer code.
+
+    :param csv_file: The csv file to get the reference from.
+    :param print_: Set to True to output the sample to the console.
+    :param temp_file: Set to False in order to avoid creating a sample file.
+    :return: None.
+    """
     with open(csv_file, 'r', newline='') as csv_io:
         csv_dict = csv.DictReader(csv_io)
-        pprint(next(csv_dict))
+        sample_entry = next(csv_dict)
+    if print_:
+        pprint(sample_entry)
+        pprint(sample_entry.keys(), width=50)
+    if temp_file:
+        with open(DATASET_ROOT / '_sample_entry.tmp', 'w') as sample_file:
+            pprint(sample_entry, stream=sample_file)
+            print(f'Exported to {sample_file.name}')
 
 
-def extract_book_data():
+def get_field_values_from_csv(
+        csv_file: Path, field_name: str, print_=False, temp_file=True):
+    """Get all values for a field to use as reference while writing importer
+    code.
+
+    :param csv_file: The csv file to get the reference from.
+    :param field_name: The name of the field for which to get the values.
+    :param print_: Set to True to output the sample to the console.
+    :param temp_file: Set to False in order to avoid creating a sample file.
+    :return: None.
+    """
+    with open(csv_file, 'r', newline='') as csv_dataset:
+        csv_dict = csv.DictReader(csv_dataset)
+        values = sorted(set(csv_entry[field_name] for csv_entry in csv_dict))
+    if print_:
+        pprint(values, width=50)
+    if temp_file:
+        temp_file_path = DATASET_ROOT / f'_values_{field_name}.tmp'
+        with open(temp_file_path, 'w') as sample_file:
+            print(*values, file=sample_file, sep='\n')
+            print(f'Exported to {sample_file.name}')
+
+
+
+def import_7k_books():
+    """Import books from a 7k books dataset csv.
+
+    7k dataset Attributions:
+        * Author : DylanCastillo
+        * Link : Kaggle_
+
+        .. _Kaggle:
+            https://www.kaggle.com/ datasets/
+            dylanjcastillo/7k-books-with-metadata
+    """
+
+    def books_data_from_dataset() -> dict:
+        with open(DATASET_7K_BOOKS, 'r', newline='') as data_source:
+            csv_dict_reader = csv.DictReader(data_source)
+            yield from csv_dict_reader
+
+    def create_book_from_dataset_data(_book_data_from_dataset: dict) -> Book:
+        return Book(
+            isbn=_book_data_from_dataset['isbn13'],
+            title=_book_data_from_dataset['title'],
+            subtitle=_book_data_from_dataset['subtitle'],
+            publication_year=int(_book_data_from_dataset['publication_year']),
+            thumbnail=_book_data_from_dataset['thumbnail'],
+            summary=_book_data_from_dataset['summary'],
+            page_count=int(_book_data_from_dataset['page_count']),
+            average_rating=Decimal(_book_data_from_dataset['average_rating']),
+            ratings_count=int(_book_data_from_dataset['ratings_count']),
+        )
+
+    for book_data_from_dataset in books_data_from_dataset():
+        book = create_book_from_dataset_data(book_data_from_dataset)
+
     def is_invalid_entry(entry: dict):
         name = entry['name']
         return (',' in name) or (';' in name)
@@ -25,7 +95,8 @@ def extract_book_data():
     def filter_book_entry(source_entry_: dict) -> dict:
         return {
             'name': source_entry_['bibliography.title'],
-            'author': source_entry_['bibliography.author.name'].replace(',', ' '),
+            'author': source_entry_['bibliography.author.name'].replace(',',
+                                                                        ' '),
             'publication_year': source_entry_['bibliography.publication.year'],
         }
 
@@ -34,7 +105,8 @@ def extract_book_data():
             csv_dict_reader = csv.DictReader(source)
             csv_dict_writer = csv.DictWriter(
                 f=filtered,
-                fieldnames=('id', 'name', 'author', 'publication_year', 'type'),
+                fieldnames=(
+                'id', 'name', 'author', 'publication_year', 'type'),
             )
             csv_dict_writer.writeheader()
             for id_ in range(1, 101):
@@ -46,76 +118,3 @@ def extract_book_data():
                 filtered_entry['id'] = str(id_)
                 filtered_entry['type'] = str(randint(1, 3))
                 csv_dict_writer.writerow(filtered_entry)
-
-
-def extract_client_data():
-    with open(CLIENT_SOURCE, 'r', newline='') as source:
-        def filter_client_entry(source_entry_: dict) -> dict:
-            return {
-                'name': ' '.join((
-                    source_entry_['First Name'],
-                    source_entry_['Last Name'])),
-                'city': source_entry_['City'],
-            }
-
-        with open(CLIENT_FILTERED, 'w', newline='') as filtered:
-            csv_dict_reader = csv.DictReader(source)
-            csv_dict_writer = csv.DictWriter(
-                f=filtered,
-                fieldnames=('id', 'name', 'city', 'age'),
-            )
-            csv_dict_writer.writeheader()
-            for id_ in range(1, 101):
-                source_entry = next(csv_dict_reader)
-                filtered_entry = filter_client_entry(source_entry)
-                filtered_entry['id'] = str(id_)
-                filtered_entry['age'] = str(randint(6, 120))
-                csv_dict_writer.writerow(filtered_entry)
-
-
-def gen_loan_data():
-    date_format = '%d/%m/%y'
-    def gen_loan_date(
-            base: datetime.datetime = datetime.datetime.now()
-    ) -> datetime.datetime:
-        recent = randint(1, 100) <= 60  # 60% chance for a recent loan
-        if recent:
-            offset = datetime.timedelta(days=randint(1, 7))
-        else:
-            offset = datetime.timedelta(weeks=randint(2, 200))
-        return base - offset
-
-    def gen_return_date(
-            loan_date_: datetime.datetime
-    ) -> datetime.datetime:
-        offset = datetime.timedelta(days=randint(1, 14))
-        return loan_date_ + offset
-
-    with open(LOAN_DATA, 'w', newline='') as loans_file:
-        csv_dict_writer = csv.DictWriter(
-            f=loans_file,
-            fieldnames=('customer id', 'book id', 'loan date', 'return date')
-        )
-        csv_dict_writer.writeheader()
-        for _ in range(100):
-            loan_date = gen_loan_date()
-            return_date = gen_return_date(loan_date)
-            entry = {
-                'customer id': str(randint(1, 100)),
-                'book id': str(randint(1, 100)),
-                'loan date': loan_date.strftime(date_format),
-                'return date': return_date.strftime(date_format)
-            }
-            csv_dict_writer.writerow(entry)
-
-
-def main():
-    # print_csv_headers(BOOK_SOURCE)
-    # extract_book_data()
-    # print_csv_headers(CLIENT_SOURCE)
-    # extract_client_data()
-    gen_loan_data()
-
-
-if __name__ == '__main__':
-    main()
